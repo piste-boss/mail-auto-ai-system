9b4541d93bd5343874d175a3bfb5d54dac38e665
piste-boss <s.ishikawa@piste-i.com>
2025-10-31 15:47:27 +0900

    Update GAS API endpoint URL

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  'https://script.google.com/macros/s/AKfycbwc_q7IQwB0eJUIL-hhDhYkPhQiap_0kppjlopldMa0VlDVllfUiO8mBqW9Ov4yIGGZ/exec';
+  'https://script.google.com/macros/s/AKfycbzn-kLRygFZgGd0JGzWPyi1rrrwjytRYIvZPpNxOTXFl4Yn768ryGt2DWErgCm6N13a/exec';
